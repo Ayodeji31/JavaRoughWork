91805076cbaf276e99c8b181175bd655865d04bd
Ayodeji Temitope Joseph <ajoseph@dunelm.com@Ayodejis-MacBook-Pro.local>
2018-11-06 16:32:50 +0000

    Calling methods

---
+++ src/roughWork/MyMethods.java
@@ -0,0 +1,26 @@
+package roughWork;
+
+public class MyMethods {
+
+	
+	int total(){
+		
+		int a_Value = 15 + 15;
+		
+		return a_Value;
+		
+	}
+	
+	void print_text(){
+		
+		System.out.println("This is so easy");
+	}
+	
+	int total (int aNumber){
+		
+		int a_Value = aNumber +20;
+		
+		return a_Value;
+	}
+	
+}
---
+++ src/roughWork/TestMethods.java
@@ -0,0 +1,20 @@
+package roughWork;
+
+public class TestMethods {
+  
+	public static void main(String[]args){
+		
+		MyMethods tap = new MyMethods();
+		
+		int aVal = tap.total();
+		
+		System.out.println( "Method result = " + aVal );
+		
+		tap.print_text();
+		int aVal2 = tap.total(30);
+		
+		System.out.println("Method result = " + aVal2 );
+		
+	}
+	
+}
